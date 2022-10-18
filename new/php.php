2a58859a437be65e508c47b2fc8c24f6d7fef567
IBRAHIMSARKER9045 <ibrahim.sarkar597@gmail.com>
2022-10-18 08:33:34 +0600

    some change

---
+++ new/php.php
@@ -0,0 +1,11 @@
+//form for student details
+<form>
+Name: <input type="text" name="name"><br>
+Roll No: <input type="text" name="roll"><br>
+Address: <input type="text" name="address"><br>
+parent name: <input type="text" name="parent"><br>
+parent mobail: <input type="text" name="mobail"><br>
+Hobby: <input type="text" name="hobby"><br>
+<input type="submit" name="submit" value="submit">
+</form>
+<?php
